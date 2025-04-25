cb5b746d45e88f5896c1e4f4975c9b8faf9a1894
KartikSaxena2004 <kartiksxn10@gmail.com>
2025-04-25 15:58:23 +0530

    Update Footer.jsx

--- React-App/src/Footer.jsx
+++ React-App/src/Footer.jsx
@@ -3,7 +3,7 @@ import "./Header.css";
 export default function Footer() {
     return (
         <div className="footer1" >
-            <h1 className="footer1" >Design and Developed By Mohit</h1>
+            <h1 className="footer1" >Design and Developed By Kartik Saxena</h1>
 
         </div>
     )}    
